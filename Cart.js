c46f27b57129905fb61288c5bc4242da54704f82
Rachit3819 <117101791+Rachit3819@users.noreply.github.com>
2023-01-17 23:50:08 +0530

    Add files via upload

---
+++ Cart.js
@@ -0,0 +1,25 @@
+import Modal from "../UI/Modal";
+import classes from "./Cart.module.css";
+function Cart(props) {
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {[{ id: 'm3', name:'Momos', amount: 2, price: 32.28 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+  return (
+    <Modal>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Amount</span>
+        <span>35.43</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]}>Add</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
+  );
+}
+export default Cart;
