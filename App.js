11585b0c86ad6873576a41dd52e1acc5e4c34224
Rachit3819 <117101791+Rachit3819@users.noreply.github.com>
2023-01-18 17:36:11 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,8 +1,22 @@
-import { Fragment } from "react";
+import { Fragment,useState } from "react";
+import Cart from "./Components/Cart/Cart";
 import Header from "./Components/Layout/Header";
+import Meals from "./Components/Meals/Meals";
+
 function App() {
+   const [cartIsOpen,setCartIsOpen]=useState(false)
+   const showCartHandler=()=>{
+    setCartIsOpen(true)
+   }
+   const hideCartHandler=()=>{
+    setCartIsOpen(false)
+   } 
   return <Fragment>
-    <Header/>
+    {cartIsOpen && <Cart onClose={hideCartHandler}/>}
+    <Header onShowCart={showCartHandler}/>
+    <main>
+      <Meals/>
+    </main>
   </Fragment>
 }
 
