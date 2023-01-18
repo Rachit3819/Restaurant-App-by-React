4f29249440357f92467b8887db679fea197b9217
Rachit3819 <117101791+Rachit3819@users.noreply.github.com>
2023-01-18 17:33:59 +0530

    Add files via upload

--- Header.js
+++ Header.js
@@ -1,18 +1,19 @@
 import { Fragment } from "react";
-import Mealsimage from '../../assets/Meals.jpg'
-import classes from './Header.module.css'
+import Mealsimage from "../../assets/Meals.jpg";
+import classes from "./Header.module.css";
 import HeaderCartButton from "./HeaderCartButton";
-
-function Header (){
-    return <Fragment>
-        <header className={classes.header}>
-            <h1>ReactMeals</h1>
-            <HeaderCartButton/>
-        </header>
-        <div className={classes['main-image']}>
-            <img src={Mealsimage}
-            alt="A table of delicious food!!!"/>
-        </div>
+function Header(props) {
+  return (
+    <Fragment>
+      <header className={classes.header} >
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCart}/>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={Mealsimage} alt="A table of delicious food!!!" />
+      </div>
+      
     </Fragment>
+  );
 }
 export default Header;
